e7c99108d4f43a5961d835e602ea35f46a1bb5e9
Pravin Chaudhari <90998082+Panya-2002@users.noreply.github.com>
2024-07-13 11:25:48 +0530

    Initial Commit

---
+++ WheatherApp/WheatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheatherAppApp.swift
+//  WheatherApp
+//
+//  Created by Pravin  on 13/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WheatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WheatherAppTests/WheatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WheatherAppTests.swift
+//  WheatherAppTests
+//
+//  Created by Pravin  on 13/07/24.
+//
+
+import Testing
+@testable import WheatherApp
+
+struct WheatherAppTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
